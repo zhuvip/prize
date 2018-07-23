2e7c8074d1684df5cf870c5ed5afb01f1987767b
zh <zh@192.168.1.111>
2018-07-23 15:06:52 +0800

    test

---
+++ src/main/java/com/prize/action/PrizeApplication.java
@@ -0,0 +1,12 @@
+package com.prize.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrizeApplication.class, args);
+	}
+}
